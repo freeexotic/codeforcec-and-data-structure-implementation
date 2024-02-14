866d871539bacffb1b437d57d4530434c3c333e3
freeexotic <vs.silaev@yandex.ru>
2024-02-14 09:41:58 +0300

    add stackarr

--- prj.lab/stackarr/stackarr.cpp
+++ prj.lab/stackarr/stackarr.cpp
@@ -1,12 +1,49 @@
+// 2024 by Polevoi Dmitry under Unlicense
+
 #include <complex/complex.hpp>
 #include <stackarr/stackarr.hpp>
 
 #include <algorithm>
 #include <stdexcept>
 
+bool StackArr::IsEmpty() const noexcept {
+  return 0 <= i_top_;
+}
 
-bool StackArr::StackIsEmpty() const noexcept {
-    if (level == -1 ){
-        return true;
-    }
+void StackArr::Pop() noexcept {
+  if (!IsEmpty()) {
+    i_top_ -= 1;
+  }
+}
+
+void StackArr::Push(const Complex& val) {
+  if (nullptr == data_) {
+    size_ = 8;
+    data_ = new Complex[size_];
+  } else if (size_ == i_top_ + 1) { 
+    auto buf = new Complex(size_ * 2);
+    std::copy(data_, data_ + size_, buf);
+    std::swap(data_, buf);
+    delete[] buf;
+    size_ *= 2;
+  }
+  data_[++i_top_] = val;
+}
+  
+Complex& StackArr::Top() {
+  if (i_top_ < 0) {
+    throw std::logic_error("StackArr - try get top form empty stack.");
+  }
+  return data_[i_top_];
+}
+
+const Complex& StackArr::Top() const {
+  if (i_top_ < 0) {
+    throw std::logic_error("StackArr - try get top form empty stack.");
+  }
+  return data_[i_top_];
+}
+
+void StackArr::Clear() noexcept {
+  i_top_ = -1;
 }
--- prj.lab/stackarr/stackarr.hpp
+++ prj.lab/stackarr/stackarr.hpp
@@ -1,27 +1,39 @@
-#ifndef STACKARR_H_240210
-#define STACKARR_H_240210
+// 2024 by Polevoi Dmitry under Unlicense
 
+#pragma once
+#ifndef STACKARR_STACKARR_HPP_20240203
+#define STACKARR_STACKARR_HPP_20240203
 
-#include <iosfwd>
+#include <cstddef>
+
+class Complex;
 
 class StackArr {
 public:
-   [[nodiscard]] StackArr() = default;
-   [[nodiscard]] StackArr(const StackArr&) = default;
-   [[nodiscard]] StackArr(int size_) : size(size_) {}
-   ~StackArr() = default; 
+  [[nodiscard]] StackArr() = default;
 
-   [[nodiscard]] StackArr& operator= (const StackArr&) = default;
-   
+  [[nodiscard]] StackArr(const StackArr&) = default;
+  
+  ~StackArr() = default;
+  
+  [[nodiscard]] StackArr& operator=(const StackArr&) = default;
 
-   int Pop() noexcept;
-   void Push( const &Complex value);
-   void Clear() noexcept;
+  bool IsEmpty() const noexcept;
+
+  void Pop() noexcept;
+
+  void Push(const Complex& val);
+  
+  [[nodiscard]] Complex& Top();
+
+  [[nodiscard]] const Complex& Top() const;
+
+  void Clear() noexcept;
+
 private:
-   int size = 0;
-   int level = -1 ;// индекс верхнего элемента (-1 - если пустой)
+  std::ptrdiff_t size_ = 0;   //!< число элементов в буфере
+  std::ptrdiff_t i_top_ = -1; //!< индекс top элемента
+  Complex* data_ = nullptr;   //!< элементы стека
 };
 
-#endif 
-
-// data null_ptr
+#endif // !STACKARR_STACKARR_HPP_20240203
