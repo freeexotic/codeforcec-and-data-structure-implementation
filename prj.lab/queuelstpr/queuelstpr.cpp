e05d75fe81ff2dcc2f88740ac6c9ddd2b0ba8893
freeexotic <vs.silaev@yandex.ru>
2024-06-26 06:34:08 +0300

    repair queuelstptr

--- prj.lab/queuelstpr/queuelstpr.cpp
+++ prj.lab/queuelstpr/queuelstpr.cpp
@@ -1,71 +1,174 @@
 #include <queuelstpr/queuelstpr.hpp>
 #include <stdexcept>
 
+QueueLstPr::QueueLstPr(const QueueLstPr& other) {
+  if (other.head_ != nullptr)
+  {
+    head_ = std::make_shared<Node>(Node(other.head_->val, nullptr));
+    std::shared_ptr<Node> a = head_;
+    std::shared_ptr<Node> b = other.head_->next;
+    while (b != nullptr)
+    {
+      std::shared_ptr<Node> tmp = std::make_shared<Node>(Node(b->val, nullptr));
+      a->next = tmp;
+      a = tmp;
+      b = b->next;
+    }
+    tail_ = a;
+    size_ = other.size_;
+  }
+}
+
+QueueLstPr::QueueLstPr(QueueLstPr&& rhs)
+  : head_(rhs.head_), tail_(rhs.tail_), size_(rhs.size_)
+{
+  rhs.head_ = rhs.tail_ = nullptr;
+  rhs.size_ = 0;
+}
+
+QueueLstPr::~QueueLstPr() {
+  while (head_ != nullptr) {
+    std::shared_ptr<Node> tmp = head_->next;
+    head_.reset();
+    head_ = tmp;
+  }
+}
+
+QueueLstPr& QueueLstPr::operator=(const QueueLstPr& rhs) {
+  if (rhs.head_ != nullptr) {
+    std::shared_ptr<Node> a = head_;
+    if (a == nullptr)
+    {
+      a = std::make_shared<Node>(Node(float(), nullptr));
+      head_ = a;
+      tail_ = a;
+    }
+    std::shared_ptr<Node> b = rhs.head_;
+    while (b != rhs.tail_) {
+      if (a == tail_)
+      {
+        a->next = std::make_shared<Node>(Node(float(), nullptr));
+        tail_ = a->next;
+      }
+      a->val = b->val;
+      a = a->next;
+      b = b->next;
+    }
+    a->val = b->val;
+    while (a->next != nullptr)
+    {
+      std::shared_ptr<Node> tmp = a->next;
+      a->next = a->next->next;
+      tmp.reset();
+    }
+    tail_ = a;
+  }
+  else {
+    while (head_ != nullptr) {
+      std::shared_ptr<Node> tmp = head_->next;
+      head_.reset();
+      head_ = tmp;
+    }
+    tail_ = nullptr;
+  }
+  size_ = rhs.size_;
+  return *this;
+}
+
+QueueLstPr& QueueLstPr::operator=(QueueLstPr&& rhs) noexcept
+{
+  std::swap(head_, rhs.head_);
+  std::swap(tail_, rhs.tail_);
+  std::swap(size_, rhs.size_);
+  return *this;
+}
+
 bool QueueLstPr::IsEmpty() const noexcept {
-    return nullptr == head_;
+  return head_ == nullptr;
 }
 
 void QueueLstPr::Pop() noexcept {
-    if (!IsEmpty()) {
-        Node* deleted = head_;
-        head_ = head_->next;
-        delete deleted;
-    }
+  if (!IsEmpty()) {
+    std::shared_ptr<Node> tmp = head_->next;
+    head_.reset();
+    head_ = tmp;
+    if (head_ == nullptr)
+      tail_ = nullptr;
+    
+    size_--;
+  }
 }
 
-void QueueLstPr::Push(const float& num) {
-    Node *f_ptr = nullptr;
-    Node *buf;
-
-    int stop = 1;
-    if (IsEmpty()) {
-        head_ = new Node(num, nullptr);
-        stop = 0;
+void QueueLstPr::Push(const float& val) {
+  if (tail_ == nullptr)
+  {
+    tail_ = std::make_shared<Node>(Node(val, nullptr));
+    head_ = tail_;
+    size_ = 1;
+  }
+  else
+  {
+    std::shared_ptr<Node> node = head_;
+    std::shared_ptr<Node> pred_node = nullptr;
+    while (node != tail_ && node->val < val)
+    {
+      pred_node = node;
+      node = node->next;
     }
 
-    Node* finder = head_;
-    if (stop == 1){
-        while(finder != nullptr){
-            if (finder->val >= num) {
-                if (f_ptr == nullptr) {
-                    buf = head_;
-                    head_ = new Node{num, buf};
-                    break;
-                }
-                else{
-                    f_ptr->next = new Node(num, finder);
-                    break;
-                }
-            }
-            else {
-                f_ptr = finder;
-                finder = finder->next;
-            }
-            if (finder == nullptr){
-                f_ptr->next = new Node(num, nullptr);
-                break;
-            }
-        }
+    if (node != tail_)
+    {
+      if (node == head_)
+      {
+        std::shared_ptr<Node> tmp = std::make_shared<Node>(Node(val, head_));
+        head_ = tmp;
+      }
+      else
+      {
+        std::shared_ptr<Node> tmp = std::make_shared<Node>(Node(val, node));
+        pred_node->next = tmp;
+      }
+    }
+    else
+    {
+      if (val < tail_->val)
+      {
+        std::shared_ptr<Node> tmp = std::make_shared<Node>(Node(val, tail_));
+        if (pred_node != nullptr) pred_node->next = tmp;
+        else head_ = tmp;
+      }
+      else
+      {
+        std::shared_ptr<Node> tmp = std::make_shared<Node>(Node(val, nullptr));
+        tail_->next = tmp;
+        tail_ = tmp;
+      }
     }
 
+    ++size_;
+  }
 }
 
 float& QueueLstPr::Top() {
-    if (IsEmpty()) {
-        throw std::logic_error("QueueLstPr - ПУСТ");
-    }
-    return head_->val;
+  if (head_ == nullptr)
+    throw std::out_of_range("Queue is empty");
+
+  return head_->val;
 }
 
 const float& QueueLstPr::Top() const {
-    if (IsEmpty()) {
-        throw std::logic_error("QueueLstPr - ПУСТ");
-    }
-    return head_->val;
+  if (head_ == nullptr)
+    throw std::out_of_range("Queue is empty");
+
+  return head_->val;
 }
 
 void QueueLstPr::Clear() noexcept {
-    while (!IsEmpty()) {
-        Pop();
-    }
+  while (head_ != nullptr) {
+    std::shared_ptr<Node> tmp = head_->next;
+    head_.reset();
+    head_ = tmp;
+  }
+  tail_ = nullptr;
+  size_ = 0;
 }
--- prj.lab/queuelstpr/queuelstpr.hpp
+++ prj.lab/queuelstpr/queuelstpr.hpp
@@ -1,43 +1,45 @@
 #pragma once
-#ifndef SILAEV_QUEUELSTPR_HPP_20240324
-#define SILAEV_QUEUELSTPR_HPP_20240324
+#ifndef QUEUELSTPR_MISIS2023S_23_32
+#define QUEUELSTPR_MISIS2023S_23_32
 
 #include <cstddef>
+#include <memory>
 
 class QueueLstPr {
 public:
-    QueueLstPr() = default;
+  QueueLstPr() = default;
 
-    QueueLstPr(const QueueLstPr&) = default;
-    QueueLstPr(QueueLstPr&&) = default;
+  QueueLstPr(const QueueLstPr&);
+  QueueLstPr(QueueLstPr&&);
 
-    ~QueueLstPr() = default;
+  ~QueueLstPr();
 
-    [[nodiscard]] QueueLstPr& operator=(const QueueLstPr&) = default;
-    [[nodiscard]] QueueLstPr& operator=(QueueLstPr&&) = default;
+  [[nodiscard]] QueueLstPr& operator=(const QueueLstPr&);
+  [[nodiscard]] QueueLstPr& operator=(QueueLstPr&&) noexcept;
 
-    [[nodiscard]] bool IsEmpty() const noexcept;
+  [[nodiscard]] bool IsEmpty() const noexcept;
 
-    void Pop() noexcept;
+  void Pop() noexcept;
 
-    void Push(const float& num);
+  void Push(const float& val);
 
-    [[nodiscard]] float& Top();
+  [[nodiscard]] float& Top();
 
-    [[nodiscard]] const float& Top() const;
+  [[nodiscard]] const float& Top() const;
 
-    void Clear() noexcept;
+  void Clear() noexcept;
 
+  int Size() { return size_; }
 private:
-    struct Node {
-        Node(const float num, Node* ptr){
-            val = num;
-            next = ptr;
-        }
-        float val;
-        Node* next = nullptr;
-    };
-    Node* head_ = nullptr;
+  struct Node {
+    Node(const float& v, std::shared_ptr<Node>n) : val(v) { next = n; }
+    float val;
+    std::shared_ptr<Node> next = nullptr;
+  };
+
+  std::shared_ptr<Node> head_ = nullptr;
+  std::shared_ptr<Node> tail_ = nullptr;
+  int size_ = 0;
 };
 
 #endif
